8752bb01347e3454218eb34f6d29460b54c13ea0
IslamusWisefool <97357305+IslamusWisefool@users.noreply.github.com>
2023-05-26 18:05:54 +0600

    Added scroll to top and modal window

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import "./modules/navbar/nav";
 import "./modules/themeHandler";
 import "./modules/btn/btn";
 import "./modules/slider/itc-slider"; // Slider
+import "./modules/scrollToTop/scrollToTop";
+import "./modules/modal/modal";
